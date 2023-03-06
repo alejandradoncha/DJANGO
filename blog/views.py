a8072128789217aeb329025c2b5d1ce0a6673dbb
alejandradoncha <alejandradoncha@gmail.com>
2023-03-06 13:07:31 +0100

    actua

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+  posts = Post.objects.filter(publishe_date__lte=timezone.nnow()).order_by('published_date')
+  return render(request, 'blog/post_list.html', {})
+
 
 # Create your views here.
